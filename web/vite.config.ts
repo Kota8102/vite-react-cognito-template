793b354b1291cf4fa479845ab533da33def54783
Kota Morimoto <kota@morimotokoutanoMacBook-Pro.local>
2025-03-25 02:17:11 +0900

    feat: ViteのReactプラグインを更新し、パッケージ依存関係を整理

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,14 @@
 import path from "node:path";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
